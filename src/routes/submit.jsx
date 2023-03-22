e1a821ba3a1b0247d1c7e295c60de0197d8e7b91
GenoWeb <evilbrain.exe@gmail.com>
2023-03-22 17:20:47 +0100

    Add submit function

--- src/routes/submit.jsx
+++ src/routes/submit.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import ProductCard from '../components/ProductCard'
-import { arrayRemove, collection, getDocs } from "firebase/firestore"; 
+import { collection, addDoc } from "firebase/firestore"; 
 import { database } from '../firebase/firebase';
 
 function Submit() {
@@ -8,10 +8,20 @@ function Submit() {
     const [price, setPrice] = useState(0)
     const [image, setImage] = useState("")
 
+    const handleSubmit = async (evt) => {
+        evt.preventDefault()
+        const docRef = await addDoc(collection(database, "products"), {
+            name,
+            price,
+            image
+        });
+        evt.target.reset()
+    }
+
     return (
         <>
             <h2 className="text-3xl">Submit a product</h2>
-            <form>
+            <form onSubmit={(evt) => handleSubmit(evt)}>
                 <label className="block my-2">Name :
                     <input className="border-2 rounded-lg ml-1 p-1" type="text" value={name} onChange={(e) => setName(e.target.value)} required/>
                 </label>
@@ -21,6 +31,7 @@ function Submit() {
                 <label className="block my-2">Image :
                     <input className="border-2 rounded-lg ml-1 p-1" type="text" value={image} onChange={(e) => setImage(e.target.value)} required/>
                 </label>
+                <button className="border cursor-pointer border-black rounded-xl lg px-3 pr-2" type="submit">envoyer</button>
             </form>
         </>
     )
